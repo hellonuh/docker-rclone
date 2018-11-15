6afa405e5b0c74235851aff25a46023f73fbbcf2
Neyazul Haque <nuh@mac.local>
2018-11-15 07:42:29 +0530

    bug fix

--- sync.sh
+++ sync.sh
@@ -15,16 +15,16 @@ if test "$(rclone ls $SYNC_SRC $RCLONE_OPTS)"; then
 
   if [ -z "$SYNC_SRC_ZIP_NAME" ]
   then
+    # the source directory is not empty
+    # it can be synced without clear data loss
+    echo "INFO: Starting rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS"
+    rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS
+  else
     echo "Zip option available"
     echo "zipping source dir"
     cd $SYNC_SRC && tar -zcvf $SYNC_SRC_ZIP_NAME . && mv $SYNC_SRC_ZIP_NAME /tmp/
     echo "Zip complete"
     rclone copy /tmp/$SYNC_SRC_ZIP_NAME $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS
-  else
-    # the source directory is not empty
-    # it can be synced without clear data loss
-    echo "INFO: Starting rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS"
-    rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS
   fi  
 
   if [ -z "$CHECK_URL" ]
